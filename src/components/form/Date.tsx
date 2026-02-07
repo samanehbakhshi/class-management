ed476f66cceca746d8e56d459ece2d10e0f55fd6
samaneh <samanehb23@gmail.com>
2026-02-07 18:59:50 +0330

    update datepicker  package import

--- src/components/form/Date.tsx
+++ src/components/form/Date.tsx
@@ -1,11 +1,9 @@
 import { cn } from "@/lib/utils/cn";
 import React from "react";
-import DatePicker from "react-multi-date-picker";
+import DatePicker, { DateObject } from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
 import "react-multi-date-picker/styles/backgrounds/bg-dark.css";
-import jalali from "react-date-object/calendars/jalali";
-import DateObject from "react-date-object";
 type DateProps = {
   className?: string;
   error?: string;
@@ -23,7 +21,7 @@ export default function DateInput({
 }: DateProps) {
   const baseClasses = cn(
     " w-full rounded-[7px] dark:bg-dark border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-gray-6 dark:text-gray-3 outline-none transition focus:border-primary active:border-primary dark:border-dark-3 dark:bg-dark-2 dark:focus:border-primary flatpickr-input active",
-    className
+    className,
   );
   return (
     <div>
