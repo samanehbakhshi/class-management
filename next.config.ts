2195da8b5cc5d5a4dd0ee673888260e12f95dc74
samaneh <samanehb23@gmail.com>
2026-02-07 20:50:59 +0330

    ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  ignoreBuildErrors: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
